5a62f5601bddaaaaf8faee2186521df25bbc1dd5
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-10-12 22:43:23 +0200

    adds mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
