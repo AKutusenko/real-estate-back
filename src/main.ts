813d0b80697c008b002b9af954a020b8691a7554
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-10-15 11:20:03 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3333, '0.0.0.0');
+  console.log('port ', process.env.PORT);
 }
 bootstrap();
