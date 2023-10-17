32702df1a4d2a30e8412f22c195cd71b4db64e5c
AKutusenko <76035623+AKutusenko@users.noreply.github.com>
2023-10-18 00:13:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(Number(process.env.PORT) || 80, '0.0.0.0');
 }
